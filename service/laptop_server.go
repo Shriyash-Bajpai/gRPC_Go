141bb32f1a7aec483a5250186eddb23965a242d2
Shriyash-Bajpai <shriyashbajpai@gmail.com>
2026-07-12 11:03:06 +0530

    created server

---
+++ service/laptop_server.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/Shriyash-Bajpai/gRPC_Go/pb"
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type LaptopServer struct {
+	Store LaptopStore
+}
+
+func NewLaptopServer(store LaptopStore) *LaptopServer {
+	return &LaptopServer{store}
+}
+
+// CreateLaptop is a unary RPC to create a new laptop
+func (server *LaptopServer) CreateLaptop(
+	ctx context.Context,
+	req *pb.CreateLaptopRequest) (*pb.CreateLaptopResponse, error) {
+
+	laptop := req.GetLaptop()
+	log.Printf("received a create laptop request with id:%s", laptop.Id)
+	if len(laptop.Id) > 0 {
+		_, err := uuid.Parse(laptop.Id)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "laptop Id is not a valid UUID:%v", err)
+		}
+	} else {
+		id, err := uuid.NewRandom()
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "Cannot generate a new laptop ID:%v", err)
+		}
+		laptop.Id = id.String()
+	}
+	// save the laptop to in memory storage
+	err := server.Store.Save(laptop)
+	if err != nil {
+		code := codes.Internal
+		if errors.Is(err, ErrAlreadyExists) {
+			code = codes.AlreadyExists
+		}
+
+		return nil, status.Errorf(code, "cannot save laptop to the store:%v", err)
+	}
+	log.Printf("saved laptop with id:%s", laptop.Id)
+	res := &pb.CreateLaptopResponse{
+		Id: laptop.Id,
+	}
+	return res, nil
+}
